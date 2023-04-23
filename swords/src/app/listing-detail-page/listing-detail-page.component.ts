650fbebcd2e244c9e477eeca72c0f72deefeb297
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-04-23 22:54:23 +0200

    Add files via upload

---
+++ swords/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListingsService } from '../listings.service';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-listing-detail-page',
+  templateUrl: './listing-detail-page.component.html',
+  styleUrls: ['./listing-detail-page.component.css']
+})
+export class ListingDetailPageComponent implements OnInit {
+  isLoading: boolean = true;
+  listing: Listing;
+  listings: Listing[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private listingsService: ListingsService,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listingsService.getListingById(id!).subscribe(listing => {
+      this.listing = listing;
+      this.isLoading = false;
+    })!;
+
+    this.listingsService.addViewToListing(id!).subscribe(listing =>
+      console.log('Views updated.'));
+  }
+
+  onDeleteClicked(listingId: string): void {
+    this.listingsService.deleteListing(listingId).subscribe(() => {
+      this.listings = this.listings.filter(listing => listing.id !== listingId);
+    });
+  }
+}
