c6bf12f4f8582857a2ee3c4cd751825444e97dc5
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-04-23 22:55:04 +0200

    Add files via upload

---
+++ swords/src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAF63R4HSLacDKiptZUXlY2UHIvbT0qRUw",
+        authDomain: "swords-13607.firebaseapp.com",
+        projectId: "swords-13607",
+        storageBucket: "swords-13607.appspot.com",
+        messagingSenderId: "749564939657",
+        appId: "1:749564939657:web:4e44fbf65570ec20dae558",
+        measurementId: "G-SFQJB28X6T"
+},
+
+};
